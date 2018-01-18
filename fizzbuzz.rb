d3242b7396d5a4d09c4611f5ca000bc8d084c241
Ben Matare <ben.a.matare@gmail.com>
2018-01-18 05:27:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,21 @@
 
 def fizzbuzz(int)
-
 if int % 3 == 0
   return "Fizz"
+  
 elsif int % 5 == 0
   return "Buzz"
-
+elsif int % 15 == 0
+  return "FizzBuzz"
+  
 else int % 4 == 0
   return nil
+  
 end
 end
+
+
+
+#I guess a more advanced version if it met a particular condition 
+#I could assign it a variable, and if it has 2 varibales then it would 
+#output FizzBuzz
